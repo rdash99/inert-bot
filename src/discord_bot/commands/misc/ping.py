628722d818be7013aaf53258e126a6c648d739af
Blotz <55319277+Blotz@users.noreply.github.com>
2022-06-10 16:49:20 +0000

    Updated ping due to inf rounding bug

--- src/discord_bot/commands/misc/ping.py
+++ src/discord_bot/commands/misc/ping.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from time import time
+import math
 
 
 class Ping(commands.Cog):
@@ -15,5 +16,13 @@ class Ping(commands.Cog):
         msg = await ctx.send('`pong`')
         t2 = time()
         delay = t2 - t1
-        await msg.edit(
-            content=f'`Pong! Latency is {round(delay * 1000)}ms. API Latency is {round(self.client.latency * 1000)}ms`')
+
+        if math.isinf(self.client.latency):
+            """
+            dpytest bug causes code infinity rounding error
+            """
+            await msg.edit(
+                content=f'`Pong! Latency is 0ms. API Latency is 0ms`')
+        else:
+            await msg.edit(
+                content=f'`Pong! Latency is {round(delay * 1000)}ms. API Latency is {round(self.client.latency * 1000)}ms`')
